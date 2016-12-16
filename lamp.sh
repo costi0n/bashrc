88884b7930db0ac6d740afadba7747c04a04f945
Costinel Ghita <costinel.ghita@gmail.com>
2016-12-16 10:11:34 +0100

    lamp.sh

---
+++ lamp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade
+apt-get install vim-nox apache2 dnsutils mysql-server mysql-client php5 php-pear php5-mysql php5-gd php5-cli php5-ldap php5-imap php5-curl
