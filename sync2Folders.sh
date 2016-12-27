042ba91fa3fc7eba31f5d3933928caa4f5cd2181
root <root@sysLog.aslrieti.dom>
2016-12-27 18:02:50 +0100

    sync2Folders

--- sync2Folders.sh
+++ sync2Folders.sh
@@ -9,15 +9,35 @@
 
 SRC=$1
 DST=$2
-
+NOW=$(date +%d%H%M%S)
+CWD=$(pwd)
+LOG=$CWD"/sync2Folders.log"
 RSYNC="/usr/bin/rsync"
+IIbit="7"
+
+#-------------------------------------------------------------------------
+VALUE=$(ifconfig | grep "inet" | grep -v "inet6" | \
+        grep -v "127.0.0.1" | awk -F" " '{print $2}' |\
+        awk -F":" '{print $2}' | awk -F"." '{print $2}')
 
 
+echo "---------------------------------------" >> $LOG
 if [ "$#" -ne 2 ]
   then
-    echo -en "uno o più parametri mancanti\n"
+    echo "$NOW > uno o più parametri mancanti" >> $LOG
   exit 255
 fi
 
-$RSYNC -avzh $SRC $DST
+
+reg='^[0-9]+$'
+if ! [[ $VALUE =~ $reg ]] ; then
+   echo "$NOW > si è verificato un errore, esecuzione terminata !" >> $LOG
+   exit 1
+fi
+
+if [ "$VALUE" -ne "$IIbit" ]; then
+   exit 1
+else
+   $RSYNC -avzh $SRC $DST
+fi
 
